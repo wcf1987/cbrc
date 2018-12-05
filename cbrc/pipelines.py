751f83da0d540e5e374d015a8b118a4800cfa349
ice_home <181712814@qq.com>
2018-12-05 20:49:18 +0800

    update

--- cbrc/pipelines.py
+++ cbrc/pipelines.py
@@ -15,3 +15,38 @@ class cbrcPipeline(object):
         file_name = os.path.join(r'D:\profit\PYProjects\cbrc\cbrc\CBRCresults\\',item['urltitle']+'.html')
         with open(file_name,'wb') as fp:
             fp.write(res.read())
+
+class DbSqlitePipeline(object):
+    def __init__(self):
+        """Initialize"""
+        self.__dbpool = adbapi.ConnectionPool('sqlite3',
+                database='/datebase/sqlite.db',
+                check_same_thread=False)
+    def shutdown(self):
+        """Shutdown the connection pool"""
+        self.__dbpool.close()
+    def process_item(self,item,spider):
+        """Process each item process_item"""
+        query = self.__dbpool.runInteraction(self.__insertdata, item, spider)
+        query.addErrback(self.handle_error)
+        return item
+    def __insertdata(self,tx,item,spider):
+        """Insert data into the sqlite3 database"""
+        spidername=spider.name
+        for img in item['images']:
+            tx.execute("select * from data where url = ?", (img['url'],))
+            result = tx.fetchone()
+            if result:
+                log.msg("Already exists in database", level=log.DEBUG)
+            else:
+                tx.execute(\
+                        "insert into data(url, localpath, checksum, created, spidername) values (?,?,?,?,?)",(
+                            img['url'],
+                            img['path'],
+                            img['checksum'],
+                            time.time(),
+                            spidername)
+                        )
+                log.msg("Item stored in db", level=log.DEBUG)
+    def handle_error(self,e):
+        log.err(e)
